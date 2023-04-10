b9105fb14559312b2574a0195ead78c73a18196f
Sergei Dydin <dydin_serg@mail.ru>
2023-04-10 15:05:26 +0300

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,32 @@
+﻿// напишите программу, которая на вход принимает число и выдает сумму цифр числа.
+
+Main();
+
+void Main ()
+{
+    int num = IntRead("N");
+
+    System.Console.WriteLine($"сумма цифр числа {num} равна {Summ(num)}");
+
+}
+
+int Summ(int num)
+{
+    int summ = 0;
+    for (int i = num; i > 0; i = i/10)
+    {
+        summ = summ + i%10;
+    }
+    return summ;
+}
+
+int IntRead(string argument)
+{
+    int number;
+    System.Console.WriteLine($"введите число {argument}");
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        System.Console.WriteLine("Это не число!!!");
+    }
+    return number;
+}
